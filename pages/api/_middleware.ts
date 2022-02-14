7c419a09f42e32b2d0b09b78d4c5363c945cb2bc
hanashiro <felipeaah@gmail.com>
2022-02-14 06:44:59 -0300

    aula-05-03

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,6 +1,11 @@
 import type { NextFetchEvent, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    if (req.nextUrl.pathname === '/api/hello') {
+        return NextResponse.next().cookie('a', '123');
+    }
+
     return new Response(
         `
         <strong>Ola</strong>, Web Developers!
